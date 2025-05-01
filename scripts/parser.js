83785c40b24eafb6366c04d21d29c43b56da9e5d
Sukhrattan <sukhrattansingh100@gmail.com>
2025-05-01 20:34:11 +0530

    Added Try-catch Logic in parser.js

--- scripts/parser.js
+++ scripts/parser.js
@@ -5,46 +5,53 @@ export class parser{
         this.numerical_stack=numerical_stack;
         this.tokenized_array=tokenized_array;
     }
-
+    
     parse = () =>{
         
-        
-        this.tokenized_array.forEach(token => {
-            if(token.type === "Digit"){
-                this.numerical_stack.push(token.value);
-            }
-
-             else if(token.type === "Left_Parenthesis"){
-                this.operator_stack.push(token.value);
-             }
-             else if(token.type === "Right_Parenthesis"){
-                while(this.operator_stack.length>0 && (this.operator_stack[this.operator_stack.length-1]!="(")){
-                    this.numerical_stack.push(this.operator_stack.pop());
+        try{
+            this.tokenized_array.forEach(token => {
+                if(token.type === "Digit"){
+                    this.numerical_stack.push(token.value);
                 }
-                if(token.type === "Parenthesis_Left"){
-                    this.operator_stack.pop();
-                }
-             }
-             
-
-            else if(token.type==="Operator"){
-                
-                while(this.operator_stack.length>0 && compareOperators(token.value,this.operator_stack[this.operator_stack.length-1])){
-                    this.numerical_stack.push(this.operator_stack.pop());
+    
+                 else if(token.type === "Left_Parenthesis"){
+                    this.operator_stack.push(token.value);
                     
-                }
-                this.operator_stack.push(token.value);
-                
+                 }
+                 else if(token.type === "Right_Parenthesis"){
+                    while(this.operator_stack.length>0 && (this.operator_stack[this.operator_stack.length-1]!="(")){
+                        this.numerical_stack.push(this.operator_stack.pop());
+                    }
+                    if(token.type === "Parenthesis_Left"){
+                        this.operator_stack.pop();
+                    }
+                    
+                 }
+                 
+    
+                else if(token.type==="Operator"){
+                    
+                    while(this.operator_stack.length>0 && compareOperators(token.value,this.operator_stack[this.operator_stack.length-1])){
+                        this.numerical_stack.push(this.operator_stack.pop());
+                        
+                    }
+                    this.operator_stack.push(token.value);
+                    
+                }throw new Error("Invalid Operator");
+    
+            });
+            while(this.operator_stack.length>0){
+                this.numerical_stack.push(this.operator_stack.pop());
             }
+    
+            
+    
+            return this.numerical_stack;
 
-        });
-        while(this.operator_stack.length>0){
-            this.numerical_stack.push(this.operator_stack.pop());
+
+        }catch(error){
+            console.log(error);
         }
-
-        
-
-        return this.numerical_stack;
         
     }
     
